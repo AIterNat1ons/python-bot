01f31c29bb160356117d5626cb23660d69583d27
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-02-09 16:15:35 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('NjczNTMzODU2MTMzMDIxNzE4.Xj67iw.2ENwNLxcxTVtlWB-igwoyo7zIdU')
+client.run(os.environ['TOKEN'])
