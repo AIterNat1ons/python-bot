58e2ce4169143eedb252f7ebb2416832e160c87d
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-02-09 17:03:41 +0000

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -4,19 +4,25 @@ from discord.ext import commands
 
 class Music(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
+        def __init__(self, client):
+            self.client = client
+
+        @commands.Cog.listener()
+        async def on_ready(self):
+            print('music.py has been loaded')
 
         @commands.command(pass_context = True)
         async def join(self, ctx):
-            channel = ctx.message.author.voice.voice_channel
-            await client.join_voice_channel(channel)
+            channel = ctx.author.voice.channel
+            await channel.connect()
+            await ctx.send(f'Successfully joined {channel}.')
 
         @commands.command(pass_context = True)
         async def leave(self, ctx):
-            server = ctx.message.author
-            voice_client = client.voice_client_in(server)
-            await voice_client.disconnect()
+            channel = self.client.voice.channel
+            await ctx.voice_client.disconnect()
+            await ctx.send('Successfully left the voice channel.')
+
 
         
 
