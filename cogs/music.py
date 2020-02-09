b7c06d84f9d336606dce23e01140e5e9d52fef69
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-02-09 16:45:01 +0000

    Add files via upload

---
+++ cogs/music.py
@@ -0,0 +1,31 @@
+import time
+import discord
+from discord.ext import commands
+
+class Music(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+        @commands.command(pass_context = True)
+        async def join(self, ctx):
+            channel = ctx.message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+
+        @commands.command(pass_context = True)
+        async def leave(self, ctx):
+            server = ctx.message.author
+            voice_client = client.voice_client_in(server)
+            await voice_client.disconnect()
+
+        
+
+
+
+
+
+
+
+
+def setup(client):
+    client.add_cog(mod(client))
