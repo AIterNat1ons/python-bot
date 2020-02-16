865922fee611fc6c5a97e7a528f75dccf8ed138e
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-02-16 20:02:35 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,34 @@
+import discord
+import random
+import time
+import asyncio
+import json
+from discord.ext import commands
+
+
+class Fun(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+          print ('fun.py has been loaded')
+
+
+
+    @commands.command()
+    async def set_prefix(self, ctx, prefix):
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+
+        await ctx.send(f':white_check_mark: Successfully changed your prefix to \'{prefix}\'')
+
+        prefixes [str(ctx.guild.id)] = prefix
+
+        with open('prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent = 4)
+
+
+def setup(client):
+    client.add_cog(Fun(client))
