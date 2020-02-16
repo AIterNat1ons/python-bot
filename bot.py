a9b449fad6566043038edfb28c2a30be39735c1d
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-02-16 19:58:58 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import os
 import discord
+import json
 import random
 import time
 from discord.ext import commands
+
+def get_prefix(client, message):
+    with open('prefixes.json', 'r')as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
 
 client = commands.Bot(command_prefix = '.')
 
@@ -10,6 +17,27 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='some commands'))
     print('Bot is online!')
+    
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes [str(guild.id)] = '.'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent = 4)
+            
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent = 4)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
