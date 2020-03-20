445839429437761f2d7030518eb0003121752bad
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-03-20 16:31:48 +0000

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -14,11 +14,10 @@ class mod(commands.Cog):
 
     @commands.command()
     @has_permissions(manage_messages = True)
-    async def clear(self, ctx, amount = 5):
+    async def clear(self, ctx, amount : int):
      await ctx.channel.purge(limit = amount + 1)
-     await ctx.send(f'Deleted {amount} messages')
-     time.sleep(2)
-     await ctx.channel.purge(limit = 1)
+     await ctx.send(f'Deleted {amount} messages', delete_after = 1)
+
 
     @commands.command()
     @has_permissions(kick_members = True)
@@ -26,7 +25,7 @@ class mod(commands.Cog):
      await member.kick(reason = reason)
      await ctx.send(f'Successfully kicked {member.mention} for reason {reason}')
 
-    @commands.command(name = 'ban')
+    @commands.command()
     @has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, *, reason = None):
         await member.ban(reason = reason)
@@ -36,23 +35,27 @@ class mod(commands.Cog):
     async def kick_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send(f'Sorry {ctx.message.author.mention}, you do not have permissions to do that!')
-
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You need to specify a member to kick somebody!')
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send(f'Sorry {ctx.message.author.mention}, you do not have permissions to do that!')
-
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You need to specify a member to ban somebody!')
     @clear.error
     async def clear_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send(f'Sorry {ctx.message.author.mention}, you do not have permissions to do that!')
-
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You need to specify an amount of number to be able to delete messages!')
 
 
 
 
 
     @commands.command()
+    @has_permissions(ban_members = True)
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
@@ -64,6 +67,14 @@ class mod(commands.Cog):
                 await ctx.guild.unban(user)
                 await ctx.send (f'Succesfully unbanned {user.mention}')
                 return
+
+    @unban.error
+    async def unban_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('The member was either not found or they are not banned!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You need to specify a member to unban somebody!')
+
 
 def setup(client):
     client.add_cog(mod(client))
