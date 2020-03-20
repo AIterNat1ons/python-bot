61ee311947920a723756ca4aec39fbb4c1a578d9
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-03-20 16:34:33 +0000

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -4,9 +4,6 @@ import time
 import asyncio
 from discord.ext import commands, tasks
 
-reddit = praw.Reddit(client_id='Crw9ObcZ3_5gE6Q',
-                     client_secret='ens6sHopI8z6LHY5gcprwoC3ePY',
-                     user_agent='AIterNat1ons')
 
 
 class Image(commands.Cog):
@@ -29,15 +26,7 @@ class Image(commands.Cog):
             embed.set_image(url=member.avatar_url)
             await ctx.send(embed = embed)
 
-    @commands.command()
-    async def meme(self, ctx):
-        meme_submissions = reddit.subreddit('Dankmemes').hot()
-        post_to_pick = random.randint(1, 10)
-        for i in range(0, post_to_pick):
-            i = i
-            submission = next(x for x in meme_submissions if not x.stickied)
 
-        await ctx.send(submission.url)
 
     
 
