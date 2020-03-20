c3754e98a18daceee69187553ca6d2a898bed4d1
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-03-20 22:13:49 +0000

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -2,14 +2,23 @@ import random
 import discord
 import time
 import asyncio
+import praw
 from discord.ext import commands, tasks
 
+reddit = praw.Reddit(client_id='rw9ObcZ3_5gE6Q',
+                     client_secret='ens6sHopI8z6LHY5gcprwoC3ePY',
+                     password='azzouz2005',
+                     user_agent='testscript by /u/AIterNat1ons',
+                     username='AIterNat1ons')
+
+print(reddit.user.me())
 
 
 class Image(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -26,7 +35,17 @@ class Image(commands.Cog):
             embed.set_image(url=member.avatar_url)
             await ctx.send(embed = embed)
 
-
+    @commands.command()
+    async def meme(self, ctx):
+        meme_submissions = reddit.subreddit('Dankmemes').hot()
+        post_to_pick = random.randint(1, 10)
+        await ctx.trigger_typing()
+        for i in range(0, post_to_pick):
+            i = i
+            submission = next(x for x in meme_submissions if not x.stickied)
+            submissionembed = discord.Embed(colour = discord.Colour.red())
+            submissionembed.set_image(url = submission.url)
+        await ctx.send(embed = submissionembed)
 
     
 
