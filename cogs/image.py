99dd4a41bb4f04365903c67a4382a0e4b75ca55a
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-03-21 15:17:00 +0000

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -29,25 +29,114 @@ class Image(commands.Cog):
         if member == None:
             other=discord.Embed(color=discord.Colour.red())
             other.set_image(url=ctx.author.avatar_url)
+            other.set_footer(text = 'If you are currently on mobile and the pfp hasn\'t loaded its a glitch within discord, I\'m sorry that it does not work.')
             await ctx.send(embed = other)
         else:
             embed=discord.Embed(color=discord.Colour.red())
             embed.set_image(url=member.avatar_url)
+            embed.set_footer(text = 'If you are currently on mobile and the pfp hasn\'t loaded its a glitch within discord, I\'m sorry that it does not work.')
             await ctx.send(embed = embed)
 
     @commands.command()
+    @commands.cooldown( 1, 5, commands.BucketType.channel)
     async def meme(self, ctx):
-        meme_submissions = reddit.subreddit('Dankmemes').hot()
+        subreddits = ['dankmemes',
+                    'memes',
+                    'meirl'
+                    ]
+        meme_submissions = reddit.subreddit(random.choice(subreddits)).hot()
         post_to_pick = random.randint(1, 10)
         await ctx.trigger_typing()
         for i in range(0, post_to_pick):
             i = i
             submission = next(x for x in meme_submissions if not x.stickied)
-            submissionembed = discord.Embed(colour = discord.Colour.red())
+            submissionembed = discord.Embed(title = submission.title, colour = discord.Colour.red())
             submissionembed.set_image(url = submission.url)
+            submissionembed.set_footer(text = f'r/{submission.subreddit}')
         await ctx.send(embed = submissionembed)
 
-    
+    @commands.command()
+    @commands.cooldown( 1, 5, commands.BucketType.channel)
+    async def cat(self, ctx):
+        meme_submissions = reddit.subreddit('cats').hot()
+        post_to_pick = random.randint(1, 10)
+        await ctx.trigger_typing()
+        for i in range(0, post_to_pick):
+            i = i
+            submission = next(x for x in meme_submissions if not x.stickied)
+            submissioncembed = discord.Embed(title = submission.title, colour = discord.Colour.red())
+            submissioncembed.set_image(url = submission.url)
+            submissioncembed.set_footer(text = f'r/{submission.subreddit}')
+        await ctx.send(embed = submissioncembed)
+
+    @commands.command()
+    @commands.cooldown( 1, 5, commands.BucketType.channel)
+    async def minecraft_meme(self, ctx):
+        meme_submissions = reddit.subreddit('MinecraftMemes').hot()
+        post_to_pick = random.randint(1, 10)
+        await ctx.trigger_typing()
+        for i in range(0, post_to_pick):
+            i = i
+            submission = next(x for x in meme_submissions if not x.stickied)
+            submissionmcembed = discord.Embed(title = submission.title, colour = discord.Colour.red())
+            submissionmcembed.set_image(url = submission.url)
+            submissionmcembed.set_footer(text = f'r/{submission.subreddit}')
+        await ctx.send(embed = submissionmcembed)
+
+    @commands.command()
+    @commands.cooldown( 1, 5, commands.BucketType.channel)
+    async def roblox_meme(self, ctx):
+        meme_submissions = reddit.subreddit('ROBLOXmemes').hot()
+        post_to_pick = random.randint(1, 10)
+        await ctx.trigger_typing()
+        for i in range(0, post_to_pick):
+            i = i
+            submission = next(x for x in meme_submissions if not x.stickied)
+            submissionmcembed = discord.Embed(title = submission.title, colour = discord.Colour.red())
+            submissionmcembed.set_image(url = submission.url)
+            submissionmcembed.set_footer(text = f'r/{submission.subreddit}')
+        await ctx.send(embed = submissionmcembed)
+
+    @meme.error
+    async def meme_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            cooldownembed = discord.Embed(
+                title = 'Chill out bro, slow it down',
+                description = f'You\'ll be able to use the command in **{round(error.retry_after)} seconds**\nThe default cooldown is `5s`',
+                colour = discord.Colour.blue()
+            )
+            await ctx.send(embed = cooldownembed)
+
+    @minecraft_meme.error
+    async def minecraft_meme_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            cooldownembed = discord.Embed(
+                title = 'Chill out bro, slow it down',
+                description = f'You\'ll be able to use the command in **{round(error.retry_after)} seconds**\nThe default cooldown is `5s`',
+                colour = discord.Colour.blue()
+            )
+            await ctx.send(embed = cooldownembed)
+
+    @cat.error
+    async def cat_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            cooldownembed = discord.Embed(
+                title = 'Chill out bro, slow it down',
+                description = f'You\'ll be able to use the command in **{round(error.retry_after)} seconds**\nThe default cooldown is `5s`',
+                colour = discord.Colour.blue()
+            )
+            await ctx.send(embed = cooldownembed)
+
+    @roblox_meme.error
+    async def roblox_meme_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            cooldownembed = discord.Embed(
+                title = 'Chill out bro, slow it down',
+                description = f'You\'ll be able to use the command in **{round(error.retry_after)} seconds**\nThe default cooldown is `5s`',
+                colour = discord.Colour.blue()
+            )
+            await ctx.send(embed = cooldownembed)
+  
 
 
 
