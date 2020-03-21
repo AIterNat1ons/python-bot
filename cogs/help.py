7929d76204b73b993e9b116ed6ac08c453df4356
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-03-21 15:09:32 +0000

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -19,14 +19,14 @@ class Help(commands.Cog):
         if number == None:
             helpembed = discord.Embed(
                 title = '**Categories**',
-                description = '**fun \nmusic \nimage \nconfig \nmod**'
+                description = '**fun\nmusic\nimage \nconfig \nmod**'
             )
             helpembed.set_footer(text = 'These are the categories for the bot, if you want more information on the category do .help <category>.')
             await ctx.send(embed = helpembed)
         if number == 'fun':
             funhelpembed = discord.Embed(
                 title = '**Fun Commands!**',
-                description = '**8ball**: 8ball is a mysterious balls which will answer your yes or no question ask it any question by doing this .8ball <question>! \n**gayrate**: This will rate how gay you are or someone you specify. To do this do .gayrate <person>! \n**penis**: This will tell you how long your penis is using a machine. To do this you do .penis <person>! \n **count**:This will count down from 10 all the way to 0.'
+                description = '**8ball**: 8ball is a mysterious balls which will answer your yes or no question ask it any question by doing this .8ball <question>! \n**gayrate**: This will rate how gay you are or someone you specify. To do this do .gayrate <person>! \n**penis**: This will tell you how long your penis is using a machine. To do this you do .penis <person>! \n**count**: This will count down from 10 all the way to 0.'
             )
             funhelpembed.set_footer(text = 'This is only commands for the category \'fun\', if you want to see the rest of the categories type in only .help.')
             await ctx.send(embed = funhelpembed)
@@ -42,26 +42,26 @@ class Help(commands.Cog):
         if number == 'mod':
             modhelpembed = discord.Embed(
                 title = '**Mod Commands!**',
-                description = '**kick**: Kicks the user who you specify by .kick <member> <reason>. \n**ban**: Bans the user who you specify by .ban <member> <reason>, \n**clear**: Clears an amount of messages you specify by .clear <amount>. \n**unban**: Unbans the user that has been banned by doing .unban <member>.'
+                description = '**kick**: Kicks the user who you specify by .kick <member> <reason>. \n**ban**: Bans the user who you specify by .ban <member> <reason>. \n**clear**: Clears an amount of messages you specify by .clear <amount>. \n**unban**: Unbans the user that has been banned by doing .unban <member>.'
             )
             modhelpembed.set_footer(text = 'This is only commands for the category \'mod\', if you want to see the rest of the categories type in only .help.')
             await ctx.send(embed = modhelpembed)
 
-            if number == 'image':
-                imagehelpembed = discord.Embed(
-                    title = '**Mod Commands!**',
-                    description = '**pfp**: Shows either your pfp or a specified user\'s pfp by doing .pfp <member>.'
-                )
-                imagehelpembed.set_footer(text = 'This is only commands for the category \'image\', if you want to see the rest of the categories type in only .help.')
-                await ctx.send(embed = imagehelpembed)
-                
-            if number == 'music':
-                musichelpembed = discord.Embed(
-                    title = '**Music Commands!**',
-                    description = 'WIP'
-                )
-                musichelpembed.set_footer(text = 'This is only commands for the category \'music\', if you want to see the rest of the categories type in only .help.')
-                await ctx.send(embed = musichelpembed)               
+        if number == 'image':
+            imagehelpembed = discord.Embed(
+                title = '**Image Commands!**',
+                description = '**pfp**: Shows either your pfp or a specified user\'s pfp by doing .pfp <member>. \n**meme**: The bot will show you a random meme from reddit. \n**minecraft_meme**: This will show you a random minecraft meme from reddit! \n**roblox_meme**: This will show you a random roblox meme from reddit! \n**cat**: This will show you a random cat meme from reddit!'
+            )
+            imagehelpembed.set_footer(text = 'This is only commands for the category \'image\', if you want to see the rest of the categories type in only .help.')
+            await ctx.send(embed = imagehelpembed)
+
+        if number == 'music':
+            imagehelpembed = discord.Embed(
+                title = '**Music Commands!**',
+                description = '**play**: This will play a music in the channel you are in by doing .play <song>. \n**stop**: This will stop the music that\'s playing and will leave the voice channel. \n**pause**: This will pause the music that is currently playing in the music player. \n**resume**: This will resume the currently stopped music. \n**join**: This will join the music channel that you are in! \n**queue**: This will show you the upcoming queued songs! \n**volume**: This will change the volume of the music in percentages. \n**skip**: Skips the current music playing. \n**now_playing**: This will show you the current playing music.'
+            )
+            imagehelpembed.set_footer(text = 'This is only commands for the category \'music\', if you want to see the rest of the categories type in only .help.')
+            await ctx.send(embed = imagehelpembed)
 
 
 
